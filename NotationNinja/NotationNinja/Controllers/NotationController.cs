0d4e7fbe8ec425afe3a3e44660f944f65108ed1a
Dan <dexter.jdexter@gmail.com>
2020-06-18 08:43:05 -0500

    Initial commit project

---
+++ NotationNinja/NotationNinja/Controllers/NotationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NotationNinja.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NotationController : ControllerBase
+    {
+        private readonly ILogger<NotationController> _logger;
+
+        public NotationController(ILogger<NotationController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
